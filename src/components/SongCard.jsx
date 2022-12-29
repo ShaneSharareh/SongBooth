b127ea4eaef0c8d6c68a927f0812f7095b361109
shanesharareh <shane.s.sharareh@gmail.com>
2022-12-29 12:50:06 -0800

    more on songcard and playPause components

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,12 +3,16 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import {playPause, setActiveSong} from '../redux/features/playerSlice';
 
-const SongCard = ({song, isPlaying, activeSong, i}) => {
-  const handlePauseClick = () => {
+const SongCard = ({song,data,isPlaying, activeSong, i}) => {
 
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
   }
   const handlePlayClick = () => {
-
+    dispatch(setActiveSong({song,data,i}));
+    dispatch(playPause(true));
   }
   return (
   <div className=" flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 
