2346209fa8dd64be48340d340638f1aff94687a2
shanesharareh <shane.s.sharareh@gmail.com>
2023-01-06 01:57:27 -0800

    Implement logo and assets

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
     <>
     {/* Desktop Menu */}
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
+      <img  width="220" height="131.741" src={logo} alt="logo" className="w-full h-44 object-contain"/>
       <NavLinks/>
     </div>
     {/* mobile Sidebar */}
