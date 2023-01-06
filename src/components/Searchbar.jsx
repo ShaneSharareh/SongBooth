4f6aeaf1a6874034aeae5d75418eefa655720ec5
shanesharareh <shane.s.sharareh@gmail.com>
2023-01-05 23:40:25 -0800

    Implement Top Artist

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,15 @@
-const Searchbar = () => (
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import {FiSearch} from 'react-icons/fi'
+
+const Searchbar = () => {
+  const navigate = useNavigate(true);
+  const [searchTerm, setSearchTerm] =useState('');
+
+  const handleSubmit = (e) =>{
+    
+  }
   <div>Loader</div>
-);
+};
 
 export default Searchbar;
